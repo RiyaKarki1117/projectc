901c395eefdfe78a17b324e28fd56f8f7dad621c
Riya Karki <riyakarki49@gmai.com>
2025-05-31 20:32:53 +0545

    workwork

--- projectc.c
+++ projectc.c
@@ -50,13 +50,13 @@ int main()
 //	next1:
 //	printf("Don't have an account?");
 //	return 0;
-#include <stdio.h>
+//wtf
+/*#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 #define FILE_PATH "D:\\Riya\\pro.txt"
 
-<<<<<<< HEAD
 void remove_newline(char *str)
 {
     //str[strcspn(str, "\n")] = '\0';
@@ -177,6 +177,120 @@ int main()
             printf("Invalid option\n");
             break;
     }
+    return 0;
+}
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define FILE_PATH "D:\\Riya\\pro.txt"
+
+// Remove the newline character at the end of a string
+void remove_newline(char *str) 
+{
+    str[strcspn(str, "\n")] = '\0';
+}
+
+void signUp() 
+{
+    char email[50];
+    char password[50];
+
+    printf("------Sign Up------\n");
+
+    printf("Enter New Email:\n");
+    fgets(email, sizeof(email), stdin);
+    remove_newline(email);
+
+    printf("Enter New Password:\n");
+    fgets(password, sizeof(password), stdin);
+    remove_newline(password);
+
+    FILE *fp = fopen(FILE_PATH, "w");
+    if (fp == NULL) 
+	{
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    fprintf(fp, "%s\n%s\n", email, password);
+    fclose(fp);
+    printf("Sign Up successful!\n\n");
+}
+
+int login() 
+{
+    char email[50];
+    char password[50];
+    char stored_email[50];
+    char stored_password[50];
+
+    FILE *fp = fopen(FILE_PATH, "r");
+    if (fp == NULL) 
+	{
+        printf("No account found. Please sign up first.\n");
+        return 1;
+    }
+
+    fgets(stored_email, sizeof(stored_email), fp);
+    remove_newline(stored_email);
+
+    fgets(stored_password, sizeof(stored_password), fp);
+    remove_newline(stored_password);
+
+    fclose(fp);
+
+    printf("------Log in------\n");
+
+    printf("Enter Email:\n");
+    fgets(email, sizeof(email), stdin);
+    remove_newline(email);
+
+    if (strcmp(email, stored_email) != 0) 
+	{
+        printf("Incorrect email\n");
+        return 1;
+    }
+
+    printf("Enter Password:\n");
+    fgets(password, sizeof(password), stdin);
+    remove_newline(password);
+
+    if (strcmp(password, stored_password) != 0)
+    {
+        printf("Password incorrect\n");
+        return 1;
+    }
+
+    printf("Login successful!\n");
+    return 0;
+}
+
+int main()
+{
+    int choice;
+
+    printf("1. Sign Up\n");
+    printf("2. Log In\n");
+    printf("Choose an option: ");
+    scanf("%d", &choice);
+    getchar();  // Consume the newline left by scanf
+
+    switch (choice) 
+	{
+        case 1:
+            signUp();
+            login(); // Optional: Prompt to log in after sign-up
+            break;
+        case 2:
+            login();
+            break;
+        default:
+            printf("Invalid option\n");
+            break;
+    }
+
     return 0;
 }
 
